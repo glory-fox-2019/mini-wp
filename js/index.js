281fd5cdb0d413f421db9979d892cea82f6017d1
justarya <just1arya@gmail.com>
2019-08-12 21:05:17 +0700

    revisi

--- js/index.js
+++ js/index.js
@@ -6,8 +6,11 @@ $(document).ready(function(){
   let quill = new Quill('.editor--content__wysiwyg', {
     theme: 'snow'
   });
-
+  $('#form--post').submit(function(e){
+    e.preventDefault();
+  })
   $('.editor--tag__input__form input').keyup(function(e){
+    e.preventDefault()
     let keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){
       addTag('.editor--tag__item--list',$(this).val());
@@ -27,7 +30,6 @@ function addTag(el, val){
   let html = `
   <div class="editor--tag__item">${val} <a onclick="removeTag(this)">&times;</a></div>
   `
-  console.log(val)
   $(el).append(html);
 }
 function removeTag(el){
