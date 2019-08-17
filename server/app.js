efe3333809a0f2204659dd153878d2f141c3362a
jackbloo <a.iqbal1404@gmail.com>
2019-08-18 00:54:59 +0700

    change to vuejs

--- server/app.js
+++ server/app.js
@@ -3,10 +3,16 @@ const cors = require('cors');
 const app = express()
 const port = 3000
 const articleRouter = require('./routes/article')
+const userRouter = require('./routes/user')
 const mongoose = require('mongoose');
+const morgan = require('morgan')
+const authentication = require('./middleware/authentication')
 app.use(cors())
+app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use('/users', userRouter)
+app.use(authentication)
 app.use('/articles', articleRouter)
 mongoose.connect('mongodb://localhost:27017/mini_wordpress', {useNewUrlParser: true})
 .then(data => {
@@ -14,6 +20,7 @@ mongoose.connect('mongodb://localhost:27017/mini_wordpress', {useNewUrlParser: t
 }).catch(err => {
     console.log('error')
 })
+
 app.use(function(err,req,res,next){
     res.json({
         status: res.status || 500,
--- server/routes/article.js
+++ server/routes/article.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const articleController = require('../controllers/article')
+const images = require('../helpers/images')
+const authorization = require('../middleware/authorization')
 
 router.get('/', articleController.findArticles)
-router.post('/filter', articleController.findOne)
-router.post('/create', articleController.create)
-router.patch('/update', articleController.update)
-router.delete('/delete',articleController.delete)
+router.get('/filter/:id', articleController.findOne)
+router.post('/create',images.multer.single('featured_image'), 
+images.sendUploadToGCS, articleController.create)
+router.patch('/update/:id',images.multer.single('featured_image'), 
+images.sendUploadToGCS, articleController.update)
+router.delete('/delete/:id',authorization, articleController.delete)
+router.get('/myArticles', articleController.findMine)
+
 
 module.exports = router;
