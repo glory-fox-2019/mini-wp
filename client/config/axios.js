1fcd85bc7d7098ccc9e12f6829f8c5ad94330b63
justarya <just1arya@gmail.com>
2019-08-19 02:15:52 +0700

    Change Config

--- client/config/axios.js
+++ client/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const ax = axios.create({
-  // baseURL: 'http://35.226.234.152/api',
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'http://35.226.234.152/api',
+  // baseURL: 'http://localhost:3000/api',
   headers: {
     'token': localStorage.getItem('token')
   }
