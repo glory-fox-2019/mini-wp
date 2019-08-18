993b1bebca7a9f7d41a3a2b920d31b5dbb1119ac
Josprima Sihombing <josprima.id@gmail.com>
2019-08-19 01:59:03 +0700

    Revert "Development"

--- server/helpers/hashPassword.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs')
-
-function hashPassword(password) {
-  const salt = bcrypt.genSaltSync(10)
-  return bcrypt.hashSync(password, salt)
-}
-
-
-module.exports = hashPassword
--- server/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-const route = require('express').Router()
-const userController = require('../controllers/userController')
-
-route.post('/register', userController.register)
-route.post('/login', userController.login)
-
-module.exports= route
