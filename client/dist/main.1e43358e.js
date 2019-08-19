66e82b9007efb112070b077f79dfdc6c91d36af8
mromiario <mromiario@gmail.com>
2019-08-19 08:39:05 +0700

    final mini-wp

--- client/dist/main.1e43358e.js
+++ client/dist/main.1e43358e.js
@@ -54974,7 +54974,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54094" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52600" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- server/routes/article.js
+++ server/routes/article.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const multer = require('multer')
-const uploadImage = multer({
-    dest : './uploads'
-})
+// const uploadImage = multer({
+//     dest : './uploads'
+// })
 const router = express.Router()
 const controllerArticle = require('../controllers/controllerArticle')
 const auth= require('../middleware/auth')
@@ -16,7 +16,7 @@ router.delete('/:id',auth.authorization,controllerArticle.delete)
 router.get('/:id',controllerArticle.findOne)
 router.patch('/:id',auth.authorization,controllerArticle.update)
 
-router.post('/upload',uploadImage.single('file'),controllerArticle.upload)
+// router.post('/upload',uploadImage.single('file'),controllerArticle.upload)
 
 
 module.exports = router
